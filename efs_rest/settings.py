b98134a0119f2722d39851d0e7b9d9dcdbf6b36c
mereddy00 <78505794+mereddy00@users.noreply.github.com>
2022-03-19 15:36:04 -0500

    updated settings

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
